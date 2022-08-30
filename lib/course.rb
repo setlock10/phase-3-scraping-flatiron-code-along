890a9b9635b4b94606c65429360f2c65f5b07d75
John J Setlock II <johnsetlock@gmail.com>
2022-08-30 07:38:42 -0500

    commit tree cfad4ffb39948e79cbf4868d754bc614b8c6b923

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,24 @@
+require 'pry'
+
+class Course
+    attr_accessor :title, :schedule, :description
+
+    @@all =[]
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+    
 
 
+end
+
+# binding.pry
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
+
 
 require_relative './course.rb'
 
 class Scraper
+
+  def get_page
+    Nokogiri::HTML(URI.open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    end
+ 
+    # binding.pry
+
+  end
   
   def print_courses
     self.make_courses
